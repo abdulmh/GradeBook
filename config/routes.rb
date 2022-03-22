363b3996b0436935b7712cff2a84c09f2dddffe1
abdulmh <abdul-malikh@chc.edu>
2022-03-22 01:17:33 -0700

    Added comments to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# each route consists of 3 parts: 1 HTTP method, 2. URL to web page
+# view being requested, 3. name of Ruby class method envoked when
+# URL to web page view is requested.
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # When a get request is made for the actors URL,
+  # Rails envokes the index method in the actors Ruby controller class.
+  # Rails automatically maps the index method to a web page view names
+  # index.html.erb and serves the webpage to the browser where its
+  # rendered.
+  get "grades" => "grades#index"
 end
